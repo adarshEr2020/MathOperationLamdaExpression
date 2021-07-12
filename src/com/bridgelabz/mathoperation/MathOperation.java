c911d0155a19704642ba4ea0082f8a5daf634399
adarshEr2020 <adarshprajapati31@gmail.com>
2021-07-12 09:29:26 +0530

    [Adarsh]: Added Welcome message

---
+++ src/com/bridgelabz/mathoperation/MathOperation.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.mathoperation;
+
+public class MathOperation {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Math Operation App Using Lamda Expression: ");
+	}
+}

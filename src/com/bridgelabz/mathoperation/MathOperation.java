14b4046f7d646dd8a30181efa3b4291899b76cc9
adarshEr2020 <adarshprajapati31@gmail.com>
2021-07-12 11:21:01 +0530

    [Adarsh] Added passing the lambda function showing the result of math function

--- src/com/bridgelabz/mathoperation/MathOperation.java
+++ src/com/bridgelabz/mathoperation/MathOperation.java
@@ -4,6 +4,10 @@ package com.bridgelabz.mathoperation;
 @FunctionalInterface
 interface IMathFunction {
 	int calculate(int a, int b);
+	
+	static void printResult(int a, int b, String function, IMathFunction fobj){
+		System.out.println("Result of" +function+"is"+fobj.calculate(a,b));
+	}
 }
 
 public class MathOperation {
@@ -24,6 +28,11 @@ public class MathOperation {
 		// Add Multiply two numbers using lambda expression
 		System.out.println("Addition is " + add.calculate(5,6));
 		System.out.println("Multiplication is " +multiply.calculate(20,2));
-		System.out.println("Dividion is	" +divide.calculate(20, 10));
+		System.out.println("Division is	" +divide.calculate(20, 10));
+		
+		// Passing Lambda Expression as function Parameter to Print Result using Static Function
+		IMathFunction.printResult(6,3, "Addition", add);
+		IMathFunction.printResult(6,3,"Multiplication", multiply);
+		IMathFunction.printResult(6,3,"Division",divide);
 	}
 }
